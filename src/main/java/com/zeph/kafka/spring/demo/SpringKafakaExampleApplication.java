06cf20854320fb8847913ed69a7e119855447cb7
Benwei Zhu <benweizhu@gmail.com>
2019-04-09 14:49:29 +0800

    init kafka setup

---
+++ src/main/java/com/zeph/kafka/spring/demo/SpringKafakaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.zeph.kafka.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafakaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafakaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeph/kafka/spring/demo/SpringKafakaExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zeph.kafka.spring.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringKafakaExampleApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
